f36fd3dc0134bf09d81757570524227f3ba78550
Andre Wolf <Andre77242@hotmail.de>
2023-09-22 11:40:15 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+// ! ===== IDEALGEWICHTSRECHNER ===== //
+// - 1: Daten und Input auslesen
+// - 2: Outpus erstellen
+
+function berechnen() {
+  // // Inputs und Daten auslesen
+  const größe = Number(document.querySelector("#körpergröße").value);
+  const alter = Number(document.querySelector("#alter").value);
+  const gewicht = Number(document.querySelector("#gewicht").value);
+  const körperbau = document.querySelector(
+    `input[name="körperbau"]:checked`
+  ).value;
+  const grundKcalFrau = 655.1 + 9.6 * gewicht + 1.8 * größe - 4.7 * alter;
+  console.log(grundKcalFrau);
+  const grundKjFrau = grundKcalFrau * 4.184;
+  console.log(grundKjFrau);
+  const grundKcalMann = 66.47 + 13.7 * gewicht + 5 * größe - 6.8 * alter;
+  console.log(grundKcalMann);
+  const grundKjMann = grundKcalMann * 4.184;
+  console.log(grundKjMann);
+
+  // // Outputs
+  const arbeit = document.querySelector("#arbeit").value;
+  const kcal = document.querySelector(".kcal");
+  const kj = document.querySelector(".kj");
+  const grundumsatz = document.querySelector(".grundumsatz");
+  const gesamtumsatz = document.querySelector(".gesamtumsatz");
+  const outputGrundKcal = document.querySelector(".outputGrundKcal");
+  const outputGrundKJ = document.querySelector(".outputGrundKJ");
+  const outputGesamtKcal = document.querySelector(".outputGesamtKcal");
+  const outputGesamtKJ = document.querySelector(".outputGesamtKJ");
+
+  if (körperbau === "frau") {
+    kcal.innerHTML = "kcal";
+    kj.innerHTML = "KJ";
+    grundumsatz.innerHTML = "Grundumsatz";
+    gesamtumsatz.innerHTML = "Gesamtumsatz";
+    outputGrundKcal.innerHTML = grundKcalFrau.toFixed(0);
+    outputGrundKJ.innerHTML = grundKjFrau.toFixed(0);
+    outputGesamtKcal.innerHTML = (grundKcalFrau * arbeit).toFixed(0);
+    outputGesamtKJ.innerHTML = (grundKjFrau * arbeit).toFixed(0);
+  } else {
+    kcal.innerHTML = "kcal";
+    kj.innerHTML = "KJ";
+    grundumsatz.innerHTML = "Grundumsatz";
+    gesamtumsatz.innerHTML = "Gesamtumsatz";
+    outputGrundKcal.innerHTML = grundKcalMann.toFixed(0);
+    outputGrundKJ.innerHTML = grundKjMann.toFixed(0);
+    outputGesamtKcal.innerHTML = (grundKcalMann * arbeit).toFixed(0);
+    outputGesamtKJ.innerHTML = (grundKjMann * arbeit).toFixed(0);
+  }
+}
